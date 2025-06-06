d6134f594c9a4275751819ffc1356c409d7f92f7
Aleks Bochniak <abochniak@gmail.com>
2025-06-06 16:21:51 +0100

    Amended search sort select to append loader after selection

--- src/js/main.js
+++ src/js/main.js
@@ -5,4 +5,4 @@ import "./components/side-nav";
 import "./components/in-page-nav";
 import "./components/search.js";
 import "./components/video-banner-controls";
-import "./components/download-pdf";
+import "./components/download-pdf"; 
