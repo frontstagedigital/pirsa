77b89d13a1288ef1e7475682b141d7400a944116
Aleks Bochniak <abochniak@gmail.com>
2025-06-06 15:36:22 +0100

    New search component js

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,6 @@ import "./utils/jquery";
 import "./components/header";
 import "./components/side-nav";
 import "./components/in-page-nav";
+import "./components/search.js";
 import "./components/video-banner-controls";
 import "./components/download-pdf";
