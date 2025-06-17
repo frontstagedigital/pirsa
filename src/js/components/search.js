1178880e58789f15fbf24cce44fee4a105339fb1
Aleks Bochniak <aleksbochniak@frontstage.digital>
2025-06-17 12:33:42 +0100

    JS to open filters item on load if there is a checked option in the filter

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -1,25 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const form = document.getElementById('js-results-bar-sort');
+  const resultsBarSortform = document.getElementById('js-results-bar-sort');
 
-  if (form instanceof HTMLFormElement) {
-    const sortSelect = form.querySelector('select[name="sort"]');
+  if (resultsBarSortform instanceof HTMLFormElement) {
+    const sortSelect = resultsBarSortform.querySelector('select[name="sort"]');
 
     if (sortSelect instanceof HTMLSelectElement) {
       sortSelect.addEventListener('change', function () { 
         // Disable the select element
         //sortSelect.disabled = true; 
-
         // Create and insert the loader
         const loader = document.createElement('div');
         loader.className = 'nsw-loader nsw-m-left-xs';
         loader.innerHTML = '<span aria-hidden="true" class="nsw-loader__circle nsw-loader__circle--sm"></span>';
         sortSelect.parentNode.insertBefore(loader, sortSelect.nextSibling);
-
         // Submit the form
-        form.submit();
-        
-        
+        resultsBarSortform.submit();      
       });
     }
   }
+
+  document.querySelectorAll(".js-filters-item").forEach(function (filterItem) {
+    const filterItemCheckedBox = filterItem.querySelector(".js-filters-item-checkbox:checked");
+    if (filterItemCheckedBox) {
+      const filterItemButton = filterItem.querySelector(".js-filters-item-button");
+      if (filterItemButton && filterItemButton.getAttribute("aria-expanded") === "false") {
+        filterItemButton.click();
+      }
+    }
+  });
+
+
 });
